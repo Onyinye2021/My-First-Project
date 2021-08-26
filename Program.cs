31df5aa177e51f1b89ec2be36c0a769eb0ecab7b
Onyinye2021 <87660460+Onyinye2021@users.noreply.github.com>
2021-08-26 13:30:49 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace multiplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstnumber = 0;
+            int secondnumber = 0;
+            int thirdnumber = 0;
+            int result = 0;
+
+            Console.Write("Please enter your first number: ");
+            firstnumber = int.Parse(Console.ReadLine ());
+
+            Console.Write("Please enter the second number: ");
+            secondnumber = int.Parse(Console.ReadLine ());
+
+            Console.Write("Please enter the third number: ");
+            thirdnumber = int.Parse(Console.ReadLine ());
+
+            result = firstnumber * secondnumber * thirdnumber;
+            Console.WriteLine("The answer is: " + result);
+        }
+    }
+}
