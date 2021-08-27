ea26d1bc530cb1ba71a6a8da525b6cd7797771ac
Onyinye2021 <87660460+Onyinye2021@users.noreply.github.com>
2021-08-27 01:23:31 -0700

    Add files via upload

---
+++ Division..cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Division
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           int fisrtnumber = 0;
+           int secondnumber = 0;
+           int result = 0;
+
+           Console.Write("Please enter the first number: ");
+           fisrtnumber = int.Parse(Console.ReadLine ());
+
+           Console.Write("Please enter the second number: ");
+           secondnumber = int.Parse(Console.ReadLine ());
+
+           result = fisrtnumber / secondnumber;
+           Console.WriteLine("The answer is: " + result);
+
+
+        }
+    }
+}
