e72a792929e1f8ed7ccc82d93a0c377c1aa2e162
Onyinye2021 <87660460+Onyinye2021@users.noreply.github.com>
2021-08-27 01:22:19 -0700

    Add files via upload

---
+++ Subtraction.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace subtraction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstnumber = 0;
+            int secondnumber = 0;
+            int result = 0;
+
+            Console.Write("Please enter the first number: ");
+            firstnumber = int.Parse(Console.ReadLine ());
+
+            Console.Write("Please enter the second number: ");
+            secondnumber = int.Parse(Console.ReadLine ());
+
+            result = firstnumber - secondnumber;
+            Console.WriteLine("The answer is: " + result);
+        }
+    }
+}
